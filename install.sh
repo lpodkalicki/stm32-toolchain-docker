d1db977a01d999e6afe9e39891ae089d37b51cce
Łukasz Podkalicki <lpodkalicki@gmail.com>
2019-08-18 18:37:18 +0200

    Fix to pass properly argument list.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ docker pull ${IMAGE_NAME}:latest
 cat <<EOF > /tmp/${TOOLCHAIN_SCRIPT}.tmp
 #!/bin/bash
 
-docker run --rm --privileged -v \$(pwd):/build ${IMAGE_NAME} \$@
+docker run --rm --privileged -v \$(pwd):/build ${IMAGE_NAME} "\$@"
 EOF
 
 chmod ugo+x /tmp/${TOOLCHAIN_SCRIPT}.tmp
